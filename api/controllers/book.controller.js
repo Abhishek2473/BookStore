ce5469b453947857fc95207609c81ff7c4b21c16
Uday Kiran <udaykiranchilumula80@gmail.com>
2024-05-22 15:37:30 +0530

    book routers added

--- api/controllers/book.controller.js
+++ api/controllers/book.controller.js
@@ -21,3 +21,49 @@ export const getAllBooks = async (req, res) => {
     res.status(500).send(err.message);
   }
 };
+
+
+export const getBookById = async (req, res) => {
+  try {
+    const bookId = req.params.id;
+    const book = await Book.findById(bookId);
+
+    if (!book) {
+      return res.status(404).send('Book not found');
+    }
+
+    res.status(200).send(book);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
+
+export const updateBookById = async (req, res) => {
+  try {
+    const bookId = req.params.id;
+    const updatedBook = await Book.findByIdAndUpdate(bookId, req.body, { new: true, runValidators: true });
+
+    if (!updatedBook) {
+      return res.status(404).send('Book not found');
+    }
+
+    res.status(200).send(updatedBook);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
+
+export const deleteBookById = async (req, res) => {
+  try {
+    const bookId = req.params.id;
+    const deletedBook = await Book.findByIdAndDelete(bookId);
+
+    if (!deletedBook) {
+      return res.status(404).send('Book not found');
+    }
+
+    res.status(200).send(deletedBook);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
